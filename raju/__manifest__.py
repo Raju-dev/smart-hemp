8475fa429e44fbaf24cff4d6ebc53f7ee12a38c5
raj <rajuthapa.1012@gmail.com>
2019-12-19 22:12:24 +0530

    Added new sample model.

---
+++ raju/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Raju',
+    'version': '0.1',
+    'category': 'Purchases',
+    'website': 'https://www.odoo.com/page/purchase',
+    'description': "Hello",
+    'data': [
+    	'view.xml'
+    ],
+     'installable': True,
+    'auto_install': False
+}
